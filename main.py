574d76538ef60f166eae6963d7d14b3d3c3c9459
Matt Allen <matthew.l.allen@gmail.com>
2024-07-03 11:26:43 -0400

    Tutorial #4 completed.  Added Shop, NPC, Boss Fight.

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ rules = False
 key = False
 fight = False
 standing = True
+buy = False
+speak = False 
+boss = False
 
+name = "Matt"
 HP = 50
 HPMAX = HP
 ATK = 3
-pot = 10
-elix = 20
-gold = 0
+pot = 1
+elix = 0
+gold = 100
 x = 0
 y = 0
 
@@ -22,7 +26,7 @@ y = 0
 map = [ ["plains",	"plains",	"plains",	"plains",	"forest",	"mountain",	 "cave"],	    #y=0
         ["forest",	"forest",	"forest",	"forest",	"forest",	"hills",	"mountain"],    #y=1
         ["forest",	"fields",	"bridge",	"plains",	"hills",	"forest",	"hills"],	    #y=2
-        ["plains",	"shop",	    "town",	    "major",	"plains",	"hills",	"mountain"],	#y=3
+        ["plains",	"shop",	    "town",	    "mayor",	"plains",	"hills",	"mountain"],	#y=3
         ["plains",	"fields",	"fields",	"plains",	"hills",	"mountain",	"mountain"]]    #y=4
 
 y_len = len(map)-1
@@ -48,8 +52,8 @@ biom = {
         "shop": {
             "t": "SHOP",
              "e": False},
-        "major": {
-            "t": "MAJOR",
+        "mayor": {
+            "t": "MAYOR",
             "e": False},
         "cave": {
             "t": "CAVE",
@@ -127,8 +131,13 @@ def draw():
     print("Xx---------------------------xX")
 
 def battle():
-    global fight, play, run, HP, ATK, pot, elix, gold
-    enemy = random.choice(e_list)
+    global fight, play, run, HP, ATK, pot, elix, gold, boss
+
+    if not boss:
+        enemy = random.choice(e_list)
+    else:
+        enemy = "Dragon"
+
     hp = mobs[enemy]["hp"]
     hpmax = hp
     atk = mobs[enemy]["at"]
@@ -158,7 +167,7 @@ def battle():
 
             if hp > 0:
                 HP -= atk
-                print(enemy + " dealt " + str(ATK) + " damage to " + name + ".")
+                print(enemy + " dealt " + str(atk) + " damage to " + name + ".")
 
             input("> ")
 
@@ -167,22 +176,22 @@ def battle():
                 pot -= 1
                 heal(30)
                 HP -= atk
-                print(enemy + " dealt " + str(ATK) + " damage to " + name + ".")
-                input("< ")
+                print(enemy + " dealt " + str(atk) + " damage to " + name + ".")
+                input("> ")
             else:
                 print("No potions!")
+
+            input("> ")
 
         if choice == "3":
             if elix > 0:
                 elix -= 1
                 heal(50)
                 HP -= atk
-                print(enemy + " dealt " + str(ATK) + " damage to " + name + ".")
-                input("< ")
+                print(enemy + " dealt " + str(atk) + " damage to " + name + ".")
             else:
                 print("No elixirs!")
-
-        input("> ")
+            input("> ")
 
         if HP <= 0:
             print(enemy + " defeated " + name + "...")
@@ -191,6 +200,7 @@ def battle():
             play = False
             run = False
             print("GAME OVER")
+
             input("> ")
 
         if hp <= 0:
@@ -206,6 +216,12 @@ def battle():
             if random.randint(0, 100) < 10:
                 elix += 1
                 print("You've found an elixir!")
+            if enemy == "Dragon":
+                draw()
+                print("Congratulations, you've finished the game!")
+                boss = False
+                play = False
+                run  = False
 
             input("> ")
 
@@ -216,6 +232,99 @@ def heal(amount):
     else:
         HP = HPMAX
     print(name + "'s HP refilled to " + str(HP) + "!")
+
+def shop():
+   global buy, gold, pot, elix, ATK
+
+   while buy:
+        clear()
+        draw()
+        print("Welcome to the shop!")
+        draw()
+        print("GOLD: " + str(gold)) 
+        print("POTIONS: " + str(pot))
+        print("ELIXIRS: " + str(elix))
+        print("ATK: " + str(ATK))
+        draw()
+        print("1 - BUY POTION (30HP) -> 5 GOLD")
+        print("2 - BUY ELIXIR (50HP) -> 8 GOLD")
+        print("3 - UPGRADE WEAPON (+2ATK) -> 10 GOLD")
+        print("4 - LEAVE")
+        draw()
+
+        choice = input("# ")
+
+        if choice == "1":
+            if gold >= 5:
+                pot += 1
+                gold -= 5
+                print("You've bought a potion!")
+            else:
+                print("Not enough gold...")
+            input("> ")
+        if choice == "2":
+            if gold >= 8:
+                elix += 1
+                gold -= 8
+                print("You've bought an elixir!")
+            else:
+                print("Not enough gold...")
+            input("> ")
+        if choice == "3":
+            if gold >= 10:
+                ATK += 2
+                gold -= 10
+                print("You've upgraded your weapon!")
+            else:
+                print("Not enough gold...")
+            input("> ")
+        if choice == "4":
+            buy = False
+
+def mayor():
+    global speak, key
+
+    while speak:
+        clear()
+        draw()
+        print("Hello there, " + name + "!")
+        if ATK < 10:
+            print("You're not strong enough to face the dragon yet!  Keep practicing and come back later!")
+            key = False
+        else:
+            print("You might be able to take on the dragon now!  Take this key but be careful!!!")
+            key = True
+
+        draw()
+        print("1 - LEAVE")
+        draw()
+
+        choice = input("# ")
+
+        if choice == "1":
+            speak = False
+
+def cave():
+    global boss, key, fight
+
+    while boss:
+        clear()
+        draw()
+        print("Here lies the cave of the dragon.  What will you do?")
+        draw()
+        if key:
+            print("1 - USE KEY")
+        print("2 - TURN BACK")
+        draw()
+
+        choice = input("# ")
+
+        if choice == "1":
+            if key:
+                fight = True
+                battle()
+        elif choice == "2":
+            boss = False
 
 while run:
     while menu:
@@ -230,7 +339,6 @@ while run:
         if rules:
             print("I'm the creator of this game and these are the rules.")
             rules = False
-            choice = ""
             input("> ")
         else:
             choice = input("# ")
@@ -244,7 +352,8 @@ while run:
             load()
             clear()
             print("Welcome back, " + name + "!")
-            input("# ")
+            input("> ")
+            menu = False
             play = True
         if choice == "3":
             rules = True
@@ -285,6 +394,8 @@ while run:
                 print("5 - USE POTION (30HP)")
             if elix > 0:
                 print("6 - USE ELIXIR (50HP)")
+            if map[y][x] == "shop" or map[y][x] == "mayor" or map[y][x] == "cave":
+                print("7 - ENTER")
             draw()
 
             dest = input("# ")
@@ -325,7 +436,16 @@ while run:
                     print("No elixirs!")
                 standing = True
                 input("> ")
-
+            elif dest == "7":
+                if map[y][x] == "shop":
+                    buy = True
+                    shop()
+                if map[y][x] == "mayor":
+                    speak = True
+                    mayor()
+                if map[y][x] == "cave":
+                    boss = True
+                    cave()
             else:
                 standing = True
 
